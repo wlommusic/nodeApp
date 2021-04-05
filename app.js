317890b7ce730b04d07d1f189547deb7c556eb9b
Dipta ranjan panda <64889320+wlommusic@users.noreply.github.com>
2021-04-05 12:52:04 +0530

    update 1
    first protype

---
+++ app.js
@@ -0,0 +1,84 @@
+const http = require('http');
+const fs = require("fs");
+const _ = require("lodash");
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const { render } = require('ejs');
+const blogRoutes =  require('./routes/blogRoutes')
+
+//express app
+const app = express();
+
+//mongo connection
+const uri =
+  "mongodb+srv://dipta:panda123@nodeapp.p3coa.mongodb.net/nodeApp?retryWrites=true&w=majority";
+mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true,})
+  .then(result=> app.listen(3000))
+  .catch(err=> console.log(err));
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views','ejs');
+
+//middleware static file
+app.use(express.static('public'))
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(morgan('dev'));
+app.use((req, res, next)=>{
+  res.locals.path = req.path;
+  next();
+});
+
+//mongoose
+// app.get("/add-blog", (req, res) => {
+//   const blog = new Blog({
+//     title: "new blog 1",
+//     snippet: "about my new blog",
+//     body: "more about my new blog",
+//   });
+
+//   blog
+//     .save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+
+//routes
+
+app.get('/', (req, res)=>{
+// res.sendFile('./ejs/index.html',{root:__dirname});
+res.render('index',{title:'HOME'} );
+});
+
+app.get("/about", (req, res) => {
+  res.render('about',{title:'ABOUT'})
+});
+
+//blog routrs
+app.use('/blogs',blogRoutes)
+///
+
+app.get("/create", (req, res) => {
+  res.render("create", { title: "CREATE" });
+});
+
+
+// any redirects
+// app.get( '/url',(req,res)=>{
+//     res.redirect('/url');
+// })
+
+// 404 page
+app.use((req,res)=>{
+    res.status(404).render('404.ejs',{title:'404'});
+
+});
+ 
